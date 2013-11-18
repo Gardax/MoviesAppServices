d2b2d34a4a935ee51999a77a84f7a49859b17ae6
gardax <gardax95@gmail.com>
2013-11-19 01:14:21 +0200

    added AddMovieModel.cs

---
+++ Movies/Movies.Services/Models/AddMovieModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Services.Models
+{
+    public class AddMovieModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string CoverUrl { get; set; }
+
+        public double Rating { get; set; }
+
+        public ICollection<CategoryModel> Categories { get; set; } 
+    }
+}
