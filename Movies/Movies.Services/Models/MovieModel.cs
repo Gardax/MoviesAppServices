37fccb1aef12ed0cc62248a8c66b9111a4baeb83
gardax <gardax95@gmail.com>
2013-11-18 22:05:01 +0200

    bug fix

--- Movies/Movies.Services/Models/MovieModel.cs
+++ Movies/Movies.Services/Models/MovieModel.cs
@@ -8,6 +8,8 @@ namespace Movies.Services.Models
 {
     public class MovieModel
     {
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public string Description { get; set; }
